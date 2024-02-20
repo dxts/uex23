412eda23b01202cd5c0c2fe260800a50d7187712
dixit <dixitsabharwal05@gmail.com>
2024-02-20 18:25:15 +0100

    add base path for urls

--- src/ex-design.ts
+++ src/ex-design.ts
@@ -10,15 +10,17 @@ import './projects/context-page.js';
 import './projects/collage-page.js';
 
 const routes = [
-  { path: '/', component: 'landing-page' },
-  { path: '/empathy', component: 'empathy-page' },
-  { path: '/decomposition', component: 'decomposition-page' },
-  { path: '/analogy', component: 'analogy-page' },
-  { path: '/style', component: 'style-page' },
-  { path: '/context', component: 'context-page' },
-  { path: '/collage', component: 'collage-page' },
+  { path: '', component: 'landing-page' },
+  { path: 'empathy', component: 'empathy-page' },
+  { path: 'decomposition', component: 'decomposition-page' },
+  { path: 'analogy', component: 'analogy-page' },
+  { path: 'style', component: 'style-page' },
+  { path: 'context', component: 'context-page' },
+  { path: 'collage', component: 'collage-page' },
   { path: '(.*)', component: 'landing-page' },
 ];
 
-const router = new Router(document.querySelector('main'));
+const router = new Router(document.querySelector('main'), {
+  baseUrl: '/uex23/',
+});
 router.setRoutes(routes);
